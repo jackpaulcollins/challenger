aca67cce0f7e1e27a60a14a12f1508d5d71a2db1
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-01-29 23:31:52 -0800

    fix

--- client/src/apiRoot.js
+++ client/src/apiRoot.js
@@ -1,4 +1,4 @@
-const DEV_URL = 'http://localhost:3000/';
-const PROD_URL = 'https://ancient-forest-52932.herokuapp.com/';
+const DEV_URL = 'http://localhost:3000';
+const PROD_URL = 'https://ancient-forest-52932.herokuapp.com';
 export const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
 
