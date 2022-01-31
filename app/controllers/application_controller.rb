df85174cebc9ee74a8bbe247bf623d1f3399a1aa
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-01-30 16:41:26 -0800

    fix refresh react issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
     get :user_points, to: "reports#user_points"
     root to: "static#home"
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
