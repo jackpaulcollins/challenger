2562730946cafaeaab5406dec699a16f75f505a5
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-01 08:06:57 -0800

    turn off reporting

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -24,8 +24,8 @@ const NavigationBar = props => {
             <Button className="button" onClick={handleLogoutClick}>Logout</Button>
           </Nav>
           <Nav>
-          <Button className="button" onClick={() => navigate('/create-report')}>
-            Report Reps!
+          <Button className="button" onClick={() => navigate('/')}>
+            Reporting is complete!
           </Button>
           </Nav>
           </Container>
